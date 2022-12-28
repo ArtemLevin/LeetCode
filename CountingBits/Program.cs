29da07ccb506899cb7b551f53bbdd69851e4c63c
ArtemLevin <python_study_2022@mail.ru>
2022-12-28 22:20:24 +0300

    CountingBits

---
+++ CountingBits/Program.cs
@@ -0,0 +1,16 @@
+﻿// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
+// ans[i] is the number of 1's in the binary representation of i.
+
+Console.WriteLine("Enter an integer ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a;
+int i = 0;
+int counter = 0;
+while (a>0)
+{
+    i = a%2;
+    if(i == 1) counter++;
+    a /=2;
+
+}
+Console.WriteLine($"Number is {b} ---> {counter}");
