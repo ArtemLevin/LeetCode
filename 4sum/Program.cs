ea15a9f0ee9a411ebe67b58f862c9f78e55a02b9
ArtemLevin <python_study_2022@mail.ru>
2022-12-29 23:28:41 +0300

    FourSum

---
+++ 4sum/Program.cs
@@ -0,0 +1,53 @@
+﻿// Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+// 0 <= a, b, c, d < n
+// a, b, c, and d are distinct.
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+// You may return the answer in any order.
+
+Console.Write("Enter the length of the array ");
+int Len = int.Parse(Console.ReadLine()!);
+
+int[] OneDimArray()
+{
+    int[] array = new int[Len];
+    for (int i = 0; i < Len; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+    return array;
+}
+
+int[] array = new int[Len];
+array = OneDimArray();
+
+void PrintOneDimArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("{0}	", array[i]);
+    }
+    Console.WriteLine(" ");
+}
+
+PrintOneDimArray(array);
+
+void Quadruplet(int[] array)
+{
+    Console.Write("Enter the target ");
+    int target = int.Parse(Console.ReadLine()!);
+    int k = 0;
+    while (k != 1)
+    {
+        int a = new Random().Next(0, array.Length);
+        int b = new Random().Next(0, array.Length);
+        int c = new Random().Next(0, array.Length);
+        int d = new Random().Next(0, array.Length);
+        if (a != b && a != c && a != d && b != c && b != d && (array[a] + array[b] + array[c] + array[d] == target))
+        {
+            Console.WriteLine($"{a}, {b}, {c}, {d}");
+            k = 1;
+        }
+    }
+}
+
+Quadruplet(array);
